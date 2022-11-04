539eb235c3af220d77659b7733d235d16f65ead3
gabrielvalencia <gabev13@gmail.com>
2022-11-04 12:44:51 -0500

    Dynamic Templates completed

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -14,20 +18,33 @@ public class HelloSpringController {
 
     // Responds to /hello?name=LaunchCode
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    public String hello(@RequestParam String name) {
+    public String hello(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
         return "hello";
     }
 
     // Responds to /hello/LaunchCode
     @GetMapping ( "hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
-            return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        String AnotherGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", AnotherGreeting);
+            return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form"; // returns form.html template (.html is implied), and ResponseBody is not needed.
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
 
