d33f6720771e85ce8fed186971466967a5c0a88a
gabrielvalencia <gabev13@gmail.com>
2022-11-04 12:30:01 -0500

    Main body refactored to match demo.

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -6,42 +6,81 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by Chris Bay
  */
+
+
+
 @Controller
-@RequestMapping("hello")
 public class HelloSpringController {
 
-    //    // Handle request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Goodbye, Spring!";
+    // Responds to /hello?name=LaunchCode
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name) {
+        return "hello";
     }
 
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    // Responds to /hello/LaunchCode
+    @GetMapping ( "hello/{name}")
     @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name) {
+            return "Hello, " + name + "!";
     }
 
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
-
-    // /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "form"; // returns form.html template (.html is implied), and ResponseBody is not needed.
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+// @Controller
+// @RequestMapping("hello")
+// public class HelloSpringController {
+
+//
+//
+//
+//    // Handles requests of the form /hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "hello";
+//    }
+//
+//    // Handles requests of the form /hello/LaunchCode
+//    @GetMapping("{name}")
+//    @ResponseBody
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
+//
+//    // /hello/form
+//    @GetMapping("form")
+//    public String helloForm() {
+//        return "form"; // returns form.html template (.html is implied), and ResponseBody is not needed.
+//    }
+//
+//
+//
+//
+//
+//    //    // Handle request at path /hello
+////    @GetMapping("hello")
+////    @ResponseBody
+////    public String hello() {
+////        return "Hello, Spring!";
+////    }
+//
+//    // lives at /hello/goodbye
+//    @GetMapping("goodbye")
+//    @ResponseBody
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+// }
+// }
